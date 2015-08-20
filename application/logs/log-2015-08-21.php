8fe6e4c3a19ad7916bf5ff78f5cbd3f5a4013a8a
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2015-08-20 19:13:53 -0300

    Log-2015-08-21

---
+++ application/logs/log-2015-08-21.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2015-08-21 00:10:41 --> Config Class Initialized
+INFO - 2015-08-21 00:10:41 --> Hooks Class Initialized
+DEBUG - 2015-08-21 00:10:41 --> UTF-8 Support Enabled
+INFO - 2015-08-21 00:10:41 --> Utf8 Class Initialized
+INFO - 2015-08-21 00:10:41 --> URI Class Initialized
+DEBUG - 2015-08-21 00:10:41 --> No URI present. Default controller set.
+INFO - 2015-08-21 00:10:41 --> Router Class Initialized
+INFO - 2015-08-21 00:10:41 --> Output Class Initialized
+INFO - 2015-08-21 00:10:41 --> Security Class Initialized
+DEBUG - 2015-08-21 00:10:41 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-08-21 00:10:41 --> Input Class Initialized
+INFO - 2015-08-21 00:10:41 --> Language Class Initialized
+INFO - 2015-08-21 00:10:41 --> Loader Class Initialized
+INFO - 2015-08-21 00:10:41 --> Helper loaded: url_helper
+INFO - 2015-08-21 00:10:41 --> Helper loaded: html_helper
+INFO - 2015-08-21 00:10:41 --> Database Driver Class Initialized
+INFO - 2015-08-21 00:10:46 --> Session: Class initialized using 'database' driver.
+INFO - 2015-08-21 00:10:46 --> Model Class Initialized
+INFO - 2015-08-21 00:10:46 --> Model Class Initialized
+INFO - 2015-08-21 00:10:46 --> Controller Class Initialized
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\templates/header.php
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\templates/top_bar_menu.php
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\templates/nav_header.php
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\templates/news.php
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\templates/footer.php
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\pages/home.php
+INFO - 2015-08-21 00:10:46 --> File loaded: C:\xampp\htdocs\marciafarioli2\application\views\templates/scripts.php
+INFO - 2015-08-21 00:10:46 --> Final output sent to browser
+DEBUG - 2015-08-21 00:10:46 --> Total execution time: 4.6033
