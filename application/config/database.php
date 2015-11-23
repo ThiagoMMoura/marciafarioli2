88556c9b4d78b169b382ce6993096e515fbcddbf
Thiago Mariano de Moura <ago10_mariano@yahoo.com.br>
2015-11-23 11:43:08 -0200

    [UPD] Alteração de failover

--- application/config/database.php
+++ application/config/database.php
@@ -62,28 +62,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default']['failover'] = array(
-	array(
-		'dsn'	=> '',
-		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => 'marcia',
-		'database' => 'dbmf',
-		'dbdriver' => 'mysqli',
-		'dbprefix' => '',
-		'pconnect' => FALSE,
-		'db_debug' => TRUE,
-		'cache_on' => FALSE,
-		'cachedir' => '',
-		'char_set' => 'utf8',
-		'dbcollat' => 'utf8_general_ci',
-		'swap_pre' => '',
-		'encrypt' => FALSE,
-		'compress' => FALSE,
-		'stricton' => FALSE,
-		'save_queries' => TRUE
-	),
-	array(
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'marcia',
+	'database' => 'dbmf',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => TRUE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(
 		'dsn'	=> '',
 		'hostname' => 'mysql.hostinger.com.br',
 		'username' => 'u857456774_mf',
@@ -102,5 +99,6 @@ $db['default']['failover'] = array(
 		'compress' => FALSE,
 		'stricton' => FALSE,
 		'save_queries' => TRUE
-	)
+	),
+	'save_queries' => TRUE
 );
