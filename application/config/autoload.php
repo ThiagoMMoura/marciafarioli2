8f27d7f857c9f3737dd82fc140d848a932f7ac58
61171 <61171@3A-AD-D-27.ascar.tche.br>
2015-12-23 16:01:41 -0200

    [UPD] AutoLoad modelos de permissoes, niveis e menus

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -137,4 +137,4 @@ $autoload['language'] = array('error','warning');
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('config_model','usuario_model','menu_model');
+$autoload['model'] = array('config_model','usuario_model','menu_model','nivel_model','permissao_model');
