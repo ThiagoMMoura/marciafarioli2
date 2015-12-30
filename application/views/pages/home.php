887c7f0faf90d7b8af253f91cb9b414742c32975
61171 <61171@3A-AD-D-27.ascar.tche.br>
2015-12-30 14:20:53 -0200

    [UPD] Adição dos alertas.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,25 +1,27 @@
-﻿<?php 
+<?php
 $this->load->view('templates/nav_header', $page);
 $this->load->helper('directory');
+$this->load->view('templates/alertas');
 ?>
 <div class="row">
     <div class="small-12 small-centered columns">
-    	<? if (isset($perm) && $perm->editarhome) {
+        <? if (isset($perm) && $perm->editarhome) {
             echo '<small>' . anchor('admin/editar/carrosel', 'Editar') . '</small>';
-        }?>
+        } ?>
         <div class="carrosel">
-        	<?php
+            <?
             $pasta = './images/site/carrosel/';
-			$map = directory_map($pasta,1);
-			foreach($map as $img){ ?>
-				<div><?php echo img($pasta.$img); ?></div>
-			<?php } ?>
+            $map = directory_map($pasta, 1);
+            foreach ($map as $img) {
+                ?>
+                <div><?= img($pasta . $img); ?></div>
+            <? } ?>
         </div>
     </div>
 </div>
 <div class="row">
-  <div class="small-12 large-10 columns">
-    <?php $this->load->view('templates/news'); ?>
-  </div>
+    <div class="small-12 large-10 columns">
+        <? $this->load->view('templates/news'); ?>
+    </div>
 </div>
 <?php $this->load->view('templates/footer'); ?>
