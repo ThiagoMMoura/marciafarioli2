a0d317ccee3dbcf65bcb39d8977e92e9bedb9185
61171 <61171@3A-AD-D-27.ascar.tche.br>
2015-12-30 14:24:42 -0200

    [UPD] Adição da coluna de ações à tabela.

--- application/views/admin/usuario/nivel/busca.php
+++ application/views/admin/usuario/nivel/busca.php
@@ -1,23 +1,37 @@
-<?php ?>
+<? $this->load->view('templates/alertas'); ?>
 <div class="row">
-    <h1 class="text-center">Busca níveis de permissões para usuários</h1>
+    <h2 class="text-center">Níveis de Permissão</h2>
 </div>
 <div class="row">
-    <table class="hover">
-        <thead>
-            <tr>
-                <th width="150">Nome</th>
-                <th>Descrição</th>
-            </tr>
-        </thead>
-        <tbody>
-            <? $this->nivel_model->selecionar('*',NULL,'nome');
-            foreach($this->nivel_model->getResultados() as $nivel){ ?>
+    <div class="medium-12 medium-centered column">
+        <table class="hover">
+            <thead>
                 <tr>
-                    <th><?=$nivel->nome;?></th>
-                    <th><?=$nivel->descricao;?></th>
+                    <th width="150">Nome</th>
+                    <th>Descrição</th>
+                    <th>Ações</th>
                 </tr>
-            <? } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <? $this->nivel_model->selecionar('*',NULL,'nome');
+                foreach($this->nivel_model->getResultados() as $nivel){ ?>
+                    <tr>
+                        <td><?=$nivel->nome;?></td>
+                        <td><?=$nivel->descricao;?></td>
+                        <td>
+                            <?if($nivel->getId()==$this->config->item('nivelusuariopadrao')){
+                                if($this->session->idnivel==$nivel->getId()){
+                                    echo anchor('admin/usuario/nivel/editar/' . $nivel->getId(), 'Editar');
+                                }
+                            }else{
+                                if($this->menu_model->hasPermissao('admin-usuario-nivel-cadastro')){
+                                    echo anchor('admin/usuario/nivel/editar/' . $nivel->getId(), 'Editar');
+                                }
+                            }?>
+                        </td>
+                    </tr>
+                <? } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
