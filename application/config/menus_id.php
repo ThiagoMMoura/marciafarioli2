9f3f047c18abe48ea4a98543d529a0ae30d23fc3
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-06 13:25:20 -0200

    [INC] Config com IDs de menus por url

---
+++ application/config/menus_id.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * Lista de menus de URLs especificas e suas IDs
+ */
+$config['admin'] = 3;
+$config['admin-usuario'] = 4;
+$config['admin-usuario-nivel'] = 5;
+$config['admin_usuario_nivel_cadastro'] = 6;
