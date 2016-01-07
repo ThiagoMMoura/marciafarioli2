94ed00dee7ec702b7c4c4139bdd4b6e31f5e8957
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-07 10:11:53 -0200

    [INC] error_save e success_save

--- application/language/portuguese-brazilian/error_lang.php
+++ application/language/portuguese-brazilian/error_lang.php
@@ -14,4 +14,4 @@ $lang['error_permissao_edicao']='Você não tem permissão para editar.';
 $lang['error_permissao'] = 'Você não tem permissão para acessar está seção.';
 $lang['error_insert_failed']='Falha na inserção do registro, tente mais tarde!';
 $lang['error_album_inexistente']='O Album procurado não existe!';
-?>
+$lang['error_save']='Falha ao salvar!';
--- application/language/portuguese-brazilian/success_lang.php
+++ application/language/portuguese-brazilian/success_lang.php
@@ -9,5 +9,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['success_excluded_image']='Imagem excluida com sucesso.';
 $lang['success_excluded']='Excluido com sucesso.';
-
-?>
+$lang['success_save']='Salvo com sucesso!';
