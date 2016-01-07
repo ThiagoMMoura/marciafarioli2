a248558a6bb414fed6268f48af12f5f49360840a
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-07 15:37:56 -0200

    [INC] permissão para menu de menus do sistema

--- application/config/menus_id.php
+++ application/config/menus_id.php
@@ -9,3 +9,4 @@ $config['admin-usuario-nivel'] = 5;
 $config['admin-usuario-nivel-cadastro'] = 6;
 $config['admin-sistema'] = 7;
 $config['admin-sistema-log'] = 8;
+$config['admin-sistema-menu'] = 9;
