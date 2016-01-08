7de08a4735ef6149f6b0f133f58d1cb1d4c6baeb
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-08 15:36:50 -0200

    [INC] Função form_datalist().

--- application/helpers/MY_form_helper.php
+++ application/helpers/MY_form_helper.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-function get_form_field($form_input, $form_label = NULL, $help_text = NULL){
+function get_form_field($form_input, $form_label = NULL, $datalist = array(), $help_text = NULL){
     $form_html = '';
     
     if(!is_array($form_input)){
@@ -33,8 +33,103 @@ function get_form_field($form_input, $form_label = NULL, $help_text = NULL){
         $form_html .= $input_html;
     }
     
-    $form_html .= $form_error;
+    $form_datalist = '';
+    if(!empty($datalist)){
+        $datalist['id'] = $form_input['name'];
+        $form_datalist = form_datalist($datalist);
+    }
+    $form_html .= $form_error.$form_datalist;
     
     return $form_html;
+}
+
+/**
+* Datalist form
+*
+* @param	mixed	$data
+* @param	mixed	$options
+* @param	mixed	$selected
+* @param	mixed	$extra
+* @return	string
+*/
+function form_datalist($data = '', $options = array(),$selected = '',$extra = ''){
+    
+    $defaults = array();
+
+    if (is_array($data))
+    {
+            if (isset($data['selected']))
+            {
+                    $selected = $data['selected'];
+                    unset($data['selected']); // select tags don't have a selected attribute
+            }
+
+            if (isset($data['options']))
+            {
+                    $options = $data['options'];
+                    unset($data['options']); // select tags don't use an options attribute
+            }
+    }
+    else
+    {
+            $defaults = array('name' => $data);
+    }
+    
+    is_array($selected) OR $selected = array($selected);
+    is_array($options) OR $options = array($options);
+    
+    // If no selected state was submitted we will attempt to set it automatically
+    if (empty($selected))
+    {
+            if (is_array($data))
+            {
+                    if (isset($data['name'], $_POST[$data['name']]))
+                    {
+                            $selected = array($_POST[$data['name']]);
+                    }
+            }
+            elseif (isset($_POST[$data]))
+            {
+                    $selected = array($_POST[$data]);
+            }
+    }
+    
+    $extra = _attributes_to_string($extra);
+    
+    $multiple = (count($selected) > 1 && strpos($extra, 'multiple') === FALSE) ? ' multiple="multiple"' : '';
+    
+    $form = '<datalist '.rtrim(_parse_form_attributes($data, $defaults)).$extra.$multiple.">\n";
+    
+    foreach ($options as $key => $val)
+    {
+            $key = (string) $key;
+
+            if (is_array($val))
+            {
+                    if (empty($val))
+                    {
+                            continue;
+                    }
+
+                    $form .= '<optgroup label="'.$key."\">\n";
+
+                    foreach ($val as $optgroup_key => $optgroup_val)
+                    {
+                            $sel = in_array($optgroup_key, $selected) ? ' selected="selected"' : '';
+                            $form .= '<option value="'.html_escape($optgroup_key).'"'.$sel.'>'
+                                    .(string) $optgroup_val."</option>\n";
+                    }
+
+                    $form .= "</optgroup>\n";
+            }
+            else
+            {
+                    $form .= '<option value="'.html_escape($key).'"'
+                            .(in_array($key, $selected) ? ' selected="selected"' : '').'>'
+                            .(string) $val."</option>\n";
+            }
+    }
+
+    return $form."</datalist>\n";
 }
 
