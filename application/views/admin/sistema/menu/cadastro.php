a01a59ebf5702de54b600fd5aea9696e31406d18
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-08 15:37:50 -0200

    [UPD] Reformulação do formulário.

--- application/views/admin/sistema/menu/cadastro.php
+++ application/views/admin/sistema/menu/cadastro.php
@@ -1,63 +1,39 @@
-<?php $this->load->view('templates/alertas'); ?>
+<?php $this->load->view('templates/alertas'); 
+// Declaração de arrays de inputs , labels e hiddens.
+$hidden = array('idmenu'=>set_value('idmenu',$idmenu));
+$input['nome'] = array('name'=>'nome','placeholder'=>'Nome do menu','value'=>set_value('nome',$nome),'required'=>'');
+$label['nome'] = 'Nome Menu';
+$input['descricao'] = array('name'=>'descricao','placeholder'=>'Descreva a função do menu...','value'=>set_value('descricao',$descricao),'rows'=>4,'cols'=>300);
+$label['descricao'] = 'Descrição';
+$input['url'] = array('name'=>'url','placeholder'=>'URL do menu','value'=>set_value('url',$url));
+$label['url'] = 'URL';
+$input['grupo'] = array('name'=>'grupo','placeholder'=>'Selecione ou digite um novo Grupo','value'=>set_value('grupo',$grupo),'list'=>'grupos');
+$label['grupo'] = 'Grupo';
+?>
 <div class="row">
     <h2 class="text-center">Cadastro de Níveis</h2>
 </div>
 <div class="row">
     <div class="medium-12 medium-centered column">
-        <?= form_open('admin/sistema/menu/salvar'); ?>
-            <?php
-            $form_id = array('name'=>'idmenu','value'=>set_value('idmenu',$idmenu),'type'=>'hidden','readonly'=>'');
-            echo form_input($form_id);?>
+        <?= form_open('admin/sistema/menu/salvar','',$hidden); ?>
             <div class="row">
                 <div class="medium-12 columns">
-                    <?php
-                    $form_nome = array('name'=>'nome','placeholder'=>'Nome do menu','value'=>set_value('nome',$nome),'required'=>'');
-                    $atributos = array();
-                    if (form_error('nome') != NULL) {
-                        $atributos['class'] = isset($atributos['class']) ? $atributos['class'] . ' error' : 'error';
-                    }
-                    echo form_label('Nome Menu'.form_input($form_nome),'',$atributos);
-                    echo form_error('nome');
-                  ?>
+                    <?= get_form_field($input['nome'],$label['nome']);?>
                 </div>
             </div>
             <div class="row">
                 <div class="medium-12 columns">
-                    <?php
-                    $form_descricao = array('name'=>'descricao','placeholder'=>'Descreva a função do menu...','value'=>set_value('descricao',$descricao),'rows'=>4,'cols'=>300);
-                    $atributos = array();
-                    if (form_error('descricao') != NULL) {
-                        $atributos['class'] = isset($atributos['class']) ? $atributos['class'] . ' error' : 'error';
-                    }
-                    echo form_label('Descrição'.form_textarea($form_descricao),'',$atributos);
-                    echo form_error('descricao');
-                  ?>
+                    <?= get_form_field($input['descricao'],$label['descricao']);?>
                 </div>
             </div>
             <div class="row">
                 <div class="medium-12 columns">
-                    <?php
-                    $form_url = array('name'=>'url','placeholder'=>'URL do menu','value'=>set_value('url',$url));
-                    $atributos = array();
-                    if (form_error('url') != NULL) {
-                        $atributos['class'] = isset($atributos['class']) ? $atributos['class'] . ' error' : 'error';
-                    }
-                    echo form_label('URL'.form_input($form_url),'',$atributos);
-                    echo form_error('url');
-                  ?>
+                    <?= get_form_field($input['url'],$label['url']);?>
                 </div>
             </div>
             <div class="row">
                 <div class="medium-12 columns">
-                    <?php
-                    $form_grupo = array('name'=>'grupo','placeholder'=>'Selecione ou digite um novo Grupo','value'=>set_value('grupo',$grupo));
-                    $atributos = array();
-                    if (form_error('grupo') != NULL) {
-                        $atributos['class'] = isset($atributos['class']) ? $atributos['class'] . ' error' : 'error';
-                    }
-                    echo form_label('Grupo'.form_input($form_grupo),'',$atributos);
-                    echo form_error('grupo');
-                  ?>
+                    <?= get_form_field($input['grupo'],$label['grupo']);?>
                 </div>
             </div>
             <div class="row">
