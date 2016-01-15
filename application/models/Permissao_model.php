2b5c83684b244a09a64e146ed1ba024b03abcb2b
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-15 15:49:31 -0200

    [UPD] Adição das propriedades idrestricao e permite.

--- application/models/Permissao_model.php
+++ application/models/Permissao_model.php
@@ -8,12 +8,44 @@ if (!defined('BASEPATH')) {
  * @author Thiago Moura
  */
 class Permissao_model extends MY_Model{
-    public $idnivel;    //INT FK
-    public $idmenu;     //INT FK
-    public $consultar;  //Boolean
-    public $incluir;    //Boolean
-    public $editar;     //Boolean
-    public $excluir;    //Boolean
+    /**
+     * @var int <b>Chave Estrangeira</b> 
+     */
+    public $idnivel;
+    /**
+     * @var int <b>Chave Estrangeira</b> 
+     */
+    public $idrestricao;
+    /**
+     * @var bool 
+     */
+    public $permite;
+    
+    /**
+     * @deprecated since version 1.0
+     * @var int <b>Chave Estrangeira</b> 
+     */
+    public $idmenu;
+    /**
+     * @deprecated since version 1.0
+     * @var bool 
+     */
+    public $consultar;
+    /**
+     * @deprecated since version 1.0
+     * @var bool 
+     */
+    public $incluir;
+    /**
+     * @deprecated since version 1.0
+     * @var bool 
+     */
+    public $editar;
+    /**
+     * @deprecated since version 1.0
+     * @var bool 
+     */
+    public $excluir;
     
     public function __construct() {
         parent::__construct();
