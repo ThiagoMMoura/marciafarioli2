9524deecacb764b3d5504a1b5d60e7386295f877
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-18 15:12:08 -0200

    [UPD] Adição de novos erros.

--- application/language/portuguese-brazilian/error_lang.php
+++ application/language/portuguese-brazilian/error_lang.php
@@ -8,10 +8,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['error_login_necessario']='Você precisa realizar login para acessar a página.';
+$lang['error_login_required'] = 'Login necessário!';
 $lang['error_login_incorreto']='O email ou senha não está correto.';
 $lang['error_not_implemented']='Está função ainda não foi implementada.';
 $lang['error_permissao_edicao']='Você não tem permissão para editar.';
 $lang['error_permissao'] = 'Você não tem permissão para acessar está seção.';
-$lang['error_insert_failed']='Falha na inserção do registro, tente mais tarde!';
+$lang['error_insert_failed']='Falha na inserção do registro!';
+$lang['error_insert_failed_try_later']='Falha na inserção do registro, tente mais tarde!';
 $lang['error_album_inexistente']='O Album procurado não existe!';
 $lang['error_save']='Falha ao salvar!';
+$lang['error_register_failed'] = 'Falha ao salvar cadastro!';
+$lang['error_register_failed_try_later'] = 'Falha ao salvar cadastro, tente mais tarde!';
