9de68357be720527951c84e76a0a970b198895d5
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-18 15:33:23 -0200

    [UPD] Remoção do campo idtipologin

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -4,20 +4,35 @@ if (!defined('BASEPATH')) {
 }
 
 class Usuario_model extends MY_Model {
-
-    public $nome;           //String
-    public $email;          //String
-    public $senha;          //String
-    public $idtipologin;    /* Em Desuso */
-    public $sexo;           //String (Masculino,Feminino)
-    public $idfotoperfil;   //INT FK
-    public $idnivel;        //INT FK
+    /**
+     * @var string 
+     */
+    public $nome;
+    /**
+     * @var string 
+     */
+    public $email;
+    /**
+     * @var string 
+     */
+    public $senha;
+    /**
+     * @var string (Masculino,Feminino)
+     */
+    public $sexo;
+    /**
+     * @var int <b>Chave Estrangeira</b> 
+     */
+    public $idfotoperfil;
+    /**
+     * @var int <b>Chave Estrangeira</b> 
+     */
+    public $idnivel;
 
     public function __construct() {
         parent::__construct();
         $this->dbtable = 'usuario';
-        $this->idtipologin = $this->config->item('tipousuariopadrao');
-        $this->idnivel = $this->config->item('tipousuariopadrao');
+        $this->idnivel = $this->config->item('nivelusuariopadrao');
     }
 
     public function valida($post = TRUE) {
@@ -27,95 +42,21 @@ class Usuario_model extends MY_Model {
         } else {
             $where = array('email' => $this->email, 'senha' => $this->senha);
         }
-        $query = $this->db->get_where($this->dbtable, $where);
+        
+        $this->selecionar('*', $where);
 
-        if ($query->num_rows() == 1) {
-            $row = $query->row_array();
-            $this->setId($row['id']);
-            $this->nome = $row['nome'];
-            $this->email = $row['email'];
-            $this->idtipousuario = $row['idtipologin'];
-            $this->sexo = $row['sexo'];
-            $this->idfotoperfil = $row['idfotoperfil'];
-            $this->idnivel = $row['idnivel'];
-            return TRUE;
-        } else {
-            return FALSE;
-        }
+        return ($this->getNumRows() === 1);
     }
-
+    
+    /**
+     * @deprecated since version 0.3 Substituido pelo metódo _logged de MY_Controller
+     * @return boolean
+     */
     public function isLogado() {
         if ($this->session->has_userdata('logado') && $this->session->logado) {
             return TRUE;
         } else {
             return FALSE;
-        }
-    }
-
-    public function verificaUsuario($redireciona = TRUE) {
-        if ($this->isLogado()) {
-            return TRUE;
-        } else {
-            if ($redireciona) {
-                $this->session->set_flashdata('alerta', 'error_login_necessario');
-                redirect('login');
-            } else {
-                return FALSE;
-            }
-        }
-    }
-    
-    /**
-     * @deprecated since version 0.2
-     * @return tipo_login_model
-     */
-    public function get_permissoes() {
-        $this->load->model('tipo_login_model');
-        return $this->tipo_login_model->getObjectById($this->session->idtipologin);
-    }
-    
-    /**
-     * @deprecated since version 0.2
-     * @param string $permissao
-     * @param string $error
-     * @param boolean $redireciona
-     * @param string $pagina
-     * @return boolean
-     */
-    public function getPermissao($permissao, $error = NULL, $redireciona = FALSE, $pagina = NULL) {
-        $usuario = $this->get_permissoes();
-        if ($usuario != NULL && $usuario->{$permissao}) {
-            return TRUE;
-        } else {
-            if ($redireciona) {
-                if ($error == NULL)
-                    $error = 'error_permissao';
-                $this->session->set_flashdata('alerta', $error);
-                if ($pagina == NULL)
-                    $pagina = 'home';
-                redirect($pagina);
-            }
-            return FALSE;
-        }
-    }
-
-    public function hasPermissao($idmenu, $para = 'consultar') {
-        if (!is_numeric($idmenu)) {
-            $idmenu = $this->config->item($idmenu);
-        }
-
-        foreach ($this->session->permissoes as $permissao) {
-            if ($permissao['idmenu'] == $idmenu) {
-                return $permissao[$para];
-            }
-        }
-        return FALSE;
-    }
-
-    public function validarPermissaoDeAcesso($permissao, $alerta = 'error_permissao', $pagina = 'home') {
-        if (!$this->hasPermissao($permissao)) {
-            $this->session->set_flashdata('alerta', $alerta);
-            redirect($pagina);
         }
     }
     
