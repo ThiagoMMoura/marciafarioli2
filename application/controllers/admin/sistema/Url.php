7e46db8607aedb7edca93182c0d205089c1cac8d
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2016-01-22 00:54:48 -0200

    [INC] Controle URL

---
+++ application/controllers/admin/sistema/Url.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Description of Url
+ *
+ * @author Thiago Moura
+ */
+class Url extends MY_Controller{
+    
+    public function __construct(){
+        parent::__construct('admin/usuario/nivel');
+        $this->_set_campos_padrao();
+    }
+    
+    public function index(){
+        $this->busca();
+    }
+
+    public function busca($data = array()){
+        $this->view('busca',$data);
+    }
+    
+    public function cadastro($data = array()){
+        $this->view('cadastro',$data);
+    }
+    
+    public function editar($id = NULL){
+        $data = array();
+        if($id!==NULL){
+            $data = $this->url_model->get_array_by_id($id);
+            $data['id'] = $id;
+            return $this->cadastro($data);
+        }
+        $this->index();
+    }
+    
+    public function _set_campos_padrao() {
+        $default_page_fields = array(
+            'cadastro' => array(
+                'id' => '',
+                'nome' => '',
+                'descricao' => '',
+                'url' => ''
+            ),
+            'busca' => array(
+                'urls' => $this->url_model->selecionar('*', NULL, 'nome ASC')
+            )
+        );
+        $this->_set_default_page_fields($default_page_fields);
+    }
+
+}
