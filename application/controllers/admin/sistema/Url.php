a7f85eb5cdcdd685d84a98a557b962819edc576d
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-01-25 09:49:43 -0200

    [INC] Função salvar

--- application/controllers/admin/sistema/Url.php
+++ application/controllers/admin/sistema/Url.php
@@ -38,6 +38,29 @@ class Url extends MY_Controller{
         $this->index();
     }
     
+    public function salvar(){
+        $this->form_validation->set_rules('nome', 'Nome', 'trim|required|min_length[3]|max_length[100]');
+        $this->form_validation->set_rules('descricao', 'Descricao', 'trim|max_length[300]');
+        $this->form_validation->set_rules('url', 'URL', 'trim|required|min_length[3]|max_length[200]');
+        
+        if ($this->form_validation->run() == FALSE) {
+            $this->cadastro();
+        }else{
+            $this->url_model->setId($this->input->post('id'));
+            $this->url_model->nome = $this->input->post('nome');
+            $this->url_model->descricao = $this->input->post('descricao');
+            $this->url_model->url = $this->input->post('url');
+            $this->url_model->restricao = $this->input->post('restricao')==='restrito';
+            
+            if($this->url_model->salvar()){
+                $this->session->set_flashdata('alerta', 'success_save');
+                redirect('admin/sistema/url/busca');
+            }
+            $this->session->set_flashdata('alerta', 'error_save');
+            $this->cadastro($this->url_model->get_fields_array());
+        }
+    }
+    
     public function _set_campos_padrao() {
         $default_page_fields = array(
             'cadastro' => array(
