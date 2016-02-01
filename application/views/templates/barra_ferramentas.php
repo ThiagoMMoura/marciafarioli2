5975f9582c2db2a2a8938c7bb4a75dee0cab4949
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2016-02-01 01:03:40 -0200

    [UPD] Layout css

--- application/views/templates/barra_ferramentas.php
+++ application/views/templates/barra_ferramentas.php
@@ -2,9 +2,18 @@
     <div class="tool-bar-title">
         <label><?= strtoupper($title);?></label>
     </div>
-    <ul class="tool-bar-buttons">
+    <?php
+    $vars = array('limpar','salvar','excluir','editar','adicionar','buscar');
+    $cont = 0;
+    foreach($vars as $key => $nome){
+        if(isset(${$nome})){
+            $cont++;
+        }
+    }
+    ?>
+    <ul class="tool-bar-buttons tool-<?= $cont;?>">
         <?php if(isset($limpar)){ ?>
-        <li class="text-center right">
+        <li>
             <button type="reset" name="limpar" class="button-tool" <?= isset($limpar['extra'])?$limpar['extra']:'';?>>
                 <i class="fi-page-delete"></i>
                 <label>Limpar</label>
@@ -12,7 +21,7 @@
         </li>
         <?php } ?>
         <?php if(isset($salvar)){ ?>
-        <li class="text-center right">
+        <li>
             <button type="submit" name="salvar" class="button-tool" <?= isset($salvar['extra'])?$salvar['extra']:'';?>>
                 <i class="fi-save"></i>
                 <label>Salvar</label>
@@ -20,7 +29,7 @@
         </li>
         <?php } ?>
         <?php if(isset($excluir)){ ?>
-        <li class="text-center right">
+        <li>
             <a name="excluir" class="button-tool" href="<?= isset($excluir['href'])?base_url($excluir['href']):'#';?>" <?= isset($excluir['extra'])?$excluir['extra']:'';?>>
                 <i class="fi-trash"></i>
                 <label>Excluir</label>
@@ -28,7 +37,7 @@
         </li>
         <?php } ?>
         <?php if(isset($editar)){ ?>
-        <li class="text-center right">
+        <li>
             <a name="editar" class="button-tool" href="<?= isset($editar['href'])?base_url($editar['href']):'#';?>" <?= isset($editar['extra'])?$editar['extra']:'';?>>
                 <i class="fi-pencil"></i>
                 <label>Editar</label>
@@ -36,7 +45,7 @@
         </li>
         <?php } ?>
         <?php if(isset($adicionar)){ ?>
-        <li class="text-center right">
+        <li>
             <a name="adicionar" class="button-tool" href="<?= isset($adicionar['href'])?base_url($adicionar['href']):'#';?>" <?= isset($adicionar['extra'])?$adicionar['extra']:'';?>>
                 <i class="fi-plus"></i>
                 <label>Adicionar</label>
@@ -44,7 +53,7 @@
         </li>
         <?php } ?>
         <?php if(isset($buscar)){ ?>
-        <li class="text-center right">
+        <li>
             <a name="buscar" class="button-tool" href="<?= isset($buscar['href'])?base_url($buscar['href']):'#';?>" <?= isset($buscar['extra'])?$buscar['extra']:'';?>>
                 <i class="fi-magnifying-glass"></i>
                 <label>Buscar</label>
