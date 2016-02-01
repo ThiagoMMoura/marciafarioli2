0013d40c27db9d6b12ff93681f7e764ca1c32fe2
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2016-02-01 01:02:48 -0200

    [INC] no registration found

--- application/language/portuguese-brazilian/warning_lang.php
+++ application/language/portuguese-brazilian/warning_lang.php
@@ -10,4 +10,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lamg['warning_area_restrita_admin']='Área restrita para usuário administrador.';
 $lamg['warning_area_restrita_cliente']='Área restrita para usuário cliente.';
 $lang['warning_not_implemented']='Descupe-nos, está função ainda não foi implementada.';
-?>
+$lang['warning_no_registration_found'] = 'Nenhum registro encontrado.';
