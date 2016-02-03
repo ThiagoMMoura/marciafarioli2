aefc1b600fb406cebbad31ae20cfa1e4ca38b956
61171 <61171@3A-AD-D-27.ascar.tche.br>
2016-02-03 15:33:33 -0200

    [INC] Subir e descer menus.

--- application/views/admin/sistema/menu/busca.php
+++ application/views/admin/sistema/menu/busca.php
@@ -1,4 +1,21 @@
 <? $this->load->view('templates/alertas'); ?>
+<script type="text/javascript">
+    $(function(){
+        //função que faz a linha descer na tabela
+        $(".descer").click(function(){
+            var objLinha = $(this).parent().parent(); //Pega o objeto linha <tr>
+            $(objLinha).next().after(objLinha); //Abaixo da linha clicada, insere a linha clicada
+        });
+
+        //função que faz a linha subir na tabela
+        $(".subir").click(function(){
+            var objLinha = $(this).parent().parent(); //Pega o objeto linha <tr>
+            $(objLinha).prev().before(objLinha); //Acima da linha clicada, insere a linha clicada
+        });             
+
+    });
+
+</script>
 <div class="row">
     <div class="medium-12 medium-centered column">
         <?php
@@ -20,8 +37,21 @@
                         </tr>
                     </thead>
                     <tbody>
+                    <?php $menu_pai_anterior = -1;
+                    foreach(array_reverse($menus) as $key => $menu){
+                        $menus[$key]['pra_baixo'] = $menus[$key]['pra_cima'] = FALSE;
+                        if($menu['idmenupai']==$menu_pai_anterior){
+                            $menus[$key]['pra_baixo'] = TRUE;
+                        }
+                        if($menu['ordem']>1){
+                            $menus[$key]['pra_cima'] = TRUE;
+                        }
+                        $menu_pai_anterior = $menu['idmenupai'];
+                    }
+                    ?>
                     <?php foreach($menus as $menu){?>
-                        <tr>
+                        <tr id="<?= $menu['id'];?>">
+                            
                             <td class="visible-for-medium-up" ><?= $menu['grupo'];?></td>
                             <td><?= $menu['nome'];?></td>
                             <td class="visible-for-medium-up" ><?= $menu['descricao'];?></td>
@@ -34,7 +64,16 @@
                                 }
                             }?>
                             <td><?= $menupai;?></td>
-                            <td><?= anchor('admin/sistema/menu/editar/' . $menu['id'], 'Editar');?></td>
+                            <td><?= anchor('admin/sistema/menu/editar/' . $menu['id'], 'Editar');?>
+                                <?php
+                                    if($menu['pra_cima']){
+                                        echo '<a href="javascript:void(0)" class="subir"><i class="fi-arrow-up"></i></a>';
+                                    }
+                                    if($menu['pra_baixo']){
+                                        echo '<a href="javascript:void(0)" class="descer"><i class="fi-arrow-down"></i></a>';
+                                    }
+                                ?>
+                            </td>
                         </tr>
                     <?php }?>
                     </tbody>
