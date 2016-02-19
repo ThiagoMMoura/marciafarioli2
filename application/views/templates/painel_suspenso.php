6c09545e45ede39a4a2773da2fd68c3ce7df956e
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-02-19 15:31:32 -0200

    [INC] Template de Painel Suspenso

---
+++ application/views/templates/painel_suspenso.php
@@ -0,0 +1,6 @@
+<div id="<?= $id_painel;?>" class="reveal-modal" data-reveal aria-labelledby="<?= $id_painel;?>-title" aria-hidden="true" role="dialog">
+    <h2 id="<?= $id_painel;?>-title"><?= $painel_titulo; ?></h2>
+    <?= $conteudo; ?>
+    <a class="close-reveal-modal" aria-label="Fechar">&#215;</a>
+</div>
+
