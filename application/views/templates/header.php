3e837589795fba07db0d0bc0be04c3d7350207eb
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-03-03 10:13:51 -0300

    [UPD] favicon adicionado

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,46 +1,66 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html class="no-js" lang="pt">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Marcia Farioli - <?= $title ?></title>
-	
-    <?php 
-    $link_list = array(
-        'padrao'=>array(
-            NORMALIZE_CSS_FILE_LOCAL,
-            RESPONSIVE_FW_CSS_FILE_LOCAL,
-            FONTES_PATH . '/foundation-icons/foundation-icons.css',
-            APP_CSS_FILE_LOCAL
-            ),
-        'home'=>array(
-            SLICK_CSS_FILE_LOCAL,
-            SLICK_THEME_CSS_FILE_LOCAL,
-            CSS_PATH . '/home.css'
-            ),
-        'editar/carrosel'=>array(
-            PLUGIN_PATH . '/jcrop/css/jquery.Jcrop.min.css',
-            ),
-        'cadastro'=>array(
-            CSS_PATH .'/barra-ferramentas.css'
-            ),
-        'busca'=>array(
-            CSS_PATH .'/barra-ferramentas.css'
-            )
-        );
+    <head>
+        <?php /* Bloco Favicon */ ?>
+        <?= link_tag("images/site/favicon/favicon.ico", "icon", "image/x-icon");?>
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('images/site/favicon/apple-icon-57x57.png');?>"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('images/site/favicon/apple-icon-60x60.png');?>"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('images/site/favicon/apple-icon-72x72.png');?>"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('images/site/favicon/apple-icon-76x76.png');?>"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('images/site/favicon/apple-icon-114x114.png');?>"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('images/site/favicon/apple-icon-120x120.png');?>"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('images/site/favicon/apple-icon-144x144.png');?>"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('images/site/favicon/apple-icon-152x152.png');?>"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('images/site/favicon/apple-icon-180x180.png');?>"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="<?= base_url('images/site/favicon/android-icon-192x192.png');?>"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('images/site/favicon/favicon-32x32.png');?>"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('images/site/favicon/favicon-96x96.png');?>"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('images/site/favicon/favicon-16x16.png');?>"/>
+        <link rel="manifest" href="<?= base_url('images/site/favicon/manifest.json');?>"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="<?= base_url('images/site/favicon/ms-icon-144x144.png');?>"/>
+        <meta name="theme-color" content="#ffffff"/>
+        <?php /* FIM Bloco Favicon */ ?>
+        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Marcia Farioli - <?= $title ?></title>
 
-    foreach($link_list['padrao'] as $key => $link_item){
-            echo link_tag($link_item);
-    }
-    if(array_key_exists($page,$link_list)){
-        foreach($link_list[$page] as $key => $link_item){
-            echo link_tag($link_item);
+        <?php 
+        $link_list = array(
+            'padrao'=>array(
+                NORMALIZE_CSS_FILE_LOCAL,
+                RESPONSIVE_FW_CSS_FILE_LOCAL,
+                FONTES_PATH . '/foundation-icons/foundation-icons.css',
+                APP_CSS_FILE_LOCAL
+                ),
+            'home'=>array(
+                SLICK_CSS_FILE_LOCAL,
+                SLICK_THEME_CSS_FILE_LOCAL,
+                CSS_PATH . '/home.css'
+                ),
+            'editar/carrosel'=>array(
+                PLUGIN_PATH . '/jcrop/css/jquery.Jcrop.min.css',
+                ),
+            'cadastro'=>array(
+                CSS_PATH .'/barra-ferramentas.css'
+                ),
+            'busca'=>array(
+                CSS_PATH .'/barra-ferramentas.css'
+                )
+            );
+
+        foreach($link_list['padrao'] as $key => $link_item){
+                echo link_tag($link_item);
         }
-    }
-    
-    echo script_tag(MODERNIZR_JS_FILE_LOCAL);
-    echo script_tag(JQUERY_JS_FILE_LOCAL);
-    ?>
-</head>
+        if(array_key_exists($page,$link_list)){
+            foreach($link_list[$page] as $key => $link_item){
+                echo link_tag($link_item);
+            }
+        }
 
+        echo script_tag(MODERNIZR_JS_FILE_LOCAL);
+        echo script_tag(JQUERY_JS_FILE_LOCAL);
+        ?>
+    </head>
 <body>
