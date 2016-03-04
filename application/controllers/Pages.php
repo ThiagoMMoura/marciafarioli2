455a9f7bc0b4b18a07e051847a12ebdfe6a0739a
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-03-04 15:36:21 -0300

    [INC] set_campos_padrao

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,10 +1,10 @@
-﻿<?php
+<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Pages extends MY_Controller {
 
     public function __construct(){
         parent::__construct('pages');
-        //$this->_set_campos_padrao();
+        $this->_set_campos_padrao();
     }
     
     public function index(){
@@ -13,6 +13,19 @@ class Pages extends MY_Controller {
     
     public function alertas($data = array()){
         $this->_alertas('alerta',$data);
+    }
+
+    private function _set_campos_padrao() {
+        $page_head_elements = array(
+            'home' => array(
+                'links' => array(
+                    SLICK_CSS_FILE_LOCAL,
+                    SLICK_THEME_CSS_FILE_LOCAL,
+                    CSS_PATH . '/home.css'
+                )
+            )
+        );
+        $this->_set_page_head_elements($page_head_elements);
     }
 
 //    public function view($page = 'home'){
