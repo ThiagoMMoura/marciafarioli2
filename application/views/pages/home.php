0de0d605639e1d03a127f211dee2633958305577
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-03-11 10:24:30 -0300

    [UPD] Correção Erro no foreach quando não existem arquivos na pasta.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,23 +5,25 @@ $this->load->view('templates/alertas');
 ?>
 <div class="row">
     <div class="small-12 small-centered columns">
-        <? if (array_search('admin-pagina-home-carrosel-editar', $urls_restritas)===FALSE) {
-            echo '<small>' . anchor('admin/editar/carrosel', 'Editar') . '</small>';
+        <?php if (array_search('admin-pagina-home-carrosel-editar', $urls_restritas)===FALSE) {
+            echo '<small>' . anchor('admin/pagina/home/carrosel', 'Editar') . '</small>';
         } ?>
         <div class="carrosel">
-            <?
+            <?php
             $pasta = './images/site/pagina/home/carrosel/';
             $map = directory_map($pasta, 1);
-            foreach ($map as $img) {
-                ?>
-                <div><?= img($pasta . $img); ?></div>
-            <? } ?>
+            if(!empty($map)){
+                foreach ($map as $img) {
+                    ?>
+                    <div><?= img($pasta . $img); ?></div>
+                <?php } 
+            }?>
         </div>
     </div>
 </div>
 <div class="row">
     <div class="small-12 large-10 columns">
-        <? $this->load->view('templates/news'); ?>
+        <?php $this->load->view('templates/news'); ?>
     </div>
 </div>
 <?php $this->load->view('templates/footer'); ?>
